241355b296658649b113286282f7b88ffb8e5c8a
lpaviets <leo.paviet-salomon@ens-lyon.fr>
2021-02-03 09:41:21 +0100

    Made init.el prettier. Disabled company and irony, reactivated auto-complete

--- init.el
+++ init.el
@@ -60,16 +60,30 @@ again.")))
  '(custom-enabled-themes (quote (tsdh-dark)))
  '(custom-safe-themes
    (quote
-    ("990e24b406787568c592db2b853aa65ecc2dcd08146c0d22293259d400174e37" "9954ed41d89d2dcf601c8e7499b6bb2778180bfcaeb7cdfc648078b8e05348c6" "151bde695af0b0e69c3846500f58d9a0ca8cb2d447da68d7fbf4154dcf818ebc" "d1b4990bd599f5e2186c3f75769a2c5334063e9e541e37514942c27975700370" "fd944f09d4d0c4d4a3c82bd7b3360f17e3ada8adf29f28199d09308ba01cc092" "43c808b039893c885bdeec885b4f7572141bd9392da7f0bd8d8346e02b2ec8da" "75d3dde259ce79660bac8e9e237b55674b910b470f313cdf4b019230d01a982a" "d2e9c7e31e574bf38f4b0fb927aaff20c1e5f92f72001102758005e53d77b8c9" "a8c210aa94c4eae642a34aaf1c5c0552855dfca2153fa6dd23f3031ce19453d4" "8aca557e9a17174d8f847fb02870cb2bb67f3b6e808e46c0e54a44e3e18e1020" "a3fa4abaf08cc169b61dea8f6df1bbe4123ec1d2afeb01c17e11fdc31fc66379" "6d589ac0e52375d311afaa745205abb6ccb3b21f6ba037104d71111e7e76a3fc" "93a0885d5f46d2aeac12bf6be1754faa7d5e28b27926b8aa812840fe7d0b7983" "f0dc4ddca147f3c7b1c7397141b888562a48d9888f1595d69572db73be99a024" "6b2636879127bf6124ce541b1b2824800afc49c6ccd65439d6eb987dbf200c36" default)))
+    ("990e24b406787568c592db2b853aa65ecc2dcd08146c0d22293259d400174e37"
+     "9954ed41d89d2dcf601c8e7499b6bb2778180bfcaeb7cdfc648078b8e05348c6"
+     "151bde695af0b0e69c3846500f58d9a0ca8cb2d447da68d7fbf4154dcf818ebc"
+     "d1b4990bd599f5e2186c3f75769a2c5334063e9e541e37514942c27975700370"
+     "fd944f09d4d0c4d4a3c82bd7b3360f17e3ada8adf29f28199d09308ba01cc092"
+     "43c808b039893c885bdeec885b4f7572141bd9392da7f0bd8d8346e02b2ec8da"
+     "75d3dde259ce79660bac8e9e237b55674b910b470f313cdf4b019230d01a982a"
+     "d2e9c7e31e574bf38f4b0fb927aaff20c1e5f92f72001102758005e53d77b8c9"
+     "a8c210aa94c4eae642a34aaf1c5c0552855dfca2153fa6dd23f3031ce19453d4"
+     "8aca557e9a17174d8f847fb02870cb2bb67f3b6e808e46c0e54a44e3e18e1020"
+     "a3fa4abaf08cc169b61dea8f6df1bbe4123ec1d2afeb01c17e11fdc31fc66379"
+     "6d589ac0e52375d311afaa745205abb6ccb3b21f6ba037104d71111e7e76a3fc"
+     "93a0885d5f46d2aeac12bf6be1754faa7d5e28b27926b8aa812840fe7d0b7983"
+     "f0dc4ddca147f3c7b1c7397141b888562a48d9888f1595d69572db73be99a024"
+     "6b2636879127bf6124ce541b1b2824800afc49c6ccd65439d6eb987dbf200c36"
+     default)))
  '(fci-rule-color "#5B6268")
- '(inhibit-startup-screen t)
  '(jdee-db-active-breakpoint-face-colors (cons "#2b2a27" "#ff5d38"))
  '(jdee-db-requested-breakpoint-face-colors (cons "#2b2a27" "#98be65"))
  '(jdee-db-spec-breakpoint-face-colors (cons "#2b2a27" "#3f444a"))
  '(objed-cursor-color "#ff5d38")
  '(package-selected-packages
    (quote
-    (ggtags magit ac-math which-key pdf-tools auctex gnu-elpa-keyring-update sage-shell-mode doom-themes realgud realgud-ipdb auto-complete ##)))
+    (yasnippet-snippets yasnippet company-c-headers company-irony company function-args ggtags magit ac-math which-key pdf-tools auctex gnu-elpa-keyring-update sage-shell-mode doom-themes realgud realgud-ipdb auto-complete ##)))
  '(vc-annotate-background "#2b2a27")
  '(vc-annotate-color-map
    (list
@@ -129,11 +143,20 @@ again.")))
       (linum-mode))))
 (my-global-linum-mode t)
 
-;; Automatic start of auto-completion package
+;; Autocompletion: auto-complete, yasnippet and company
+
+;;YASnippet
+(require 'yasnippet)
+(yas-global-mode 1)
+
+;; Auto-complete
 (require 'auto-complete)
+(require 'auto-complete-config)
 (setq ac-use-quick-help t)
-(setq-default ac-sources '(ac-source-words-in-same-mode-buffers
+(setq-default ac-sources '(ac-source-yasnippet
+			   ac-source-words-in-same-mode-buffers
 			   ac-source-dictionary)) ; see auto-complete doc for other sources
+
 ;; Adding support for LaTeX auto-complete
 (require 'ac-math)
 (add-to-list 'ac-modes 'latex-mode)   ; make auto-complete aware of `latex-mode`
@@ -148,6 +171,18 @@ again.")))
 (add-hook 'LaTeX-mode-hook 'ac-LaTeX-mode-setup)
 (global-auto-complete-mode t)
 
+;; ;; Company & Irony for C/C++
+;; (require 'company)
+;; (require 'company-c-headers)
+
+;; (add-to-list 'company-backends 'company-irony)
+;; (add-hook 'after-init-hook 'global-company-mode)
+
+;; (require 'irony)
+;; (add-hook 'c++-mode-hook 'irony-mode)
+;; (add-hook 'c-mode-hook 'irony-mode)
+
+
 ;; Use pdf-tools to open PDF files
 (pdf-tools-install)
 
@@ -161,8 +196,36 @@ again.")))
 (setq TeX-source-correlate-mode t)
 (setq TeX-source-correlate-method '((dvi . source-specials) (pdf . synctex)))
 
-;; Activate ggtags in C and C++
-(add-hook 'c-mode-common-hook
-    (lambda ()
-      (when (derived-mode-p 'c-mode 'c++-mode 'java-mode 'asm-mode)
-  (ggtags-mode 1))))
+;; Configure ggtags for C and C++
+(require 'cc-mode)
+(require 'ggtags)
+
+;; (add-hook 'c-mode-common-hook
+;; 	  (lambda ()
+;; 	    (when (derived-mode-p 'c-mode 'c++-mode 'java-mode 'asm-mode)
+;; 	      (ggtags-mode 1))))
+
+;; (define-key ggtags-mode-map (kbd "C-c g s") 'ggtags-find-other-symbol)
+;; (define-key ggtags-mode-map (kbd "C-c g h") 'ggtags-view-tag-history)
+;; (define-key ggtags-mode-map (kbd "C-c g r") 'ggtags-find-reference)
+;; (define-key ggtags-mode-map (kbd "C-c g f") 'ggtags-find-file)
+;; (define-key ggtags-mode-map (kbd "C-c g c") 'ggtags-create-tags)
+;; (define-key ggtags-mode-map (kbd "C-c g u") 'ggtags-update-tags)
+
+;; (define-key ggtags-mode-map (kbd "M-,") 'pop-tag-mark)
+
+;; (setq-local imenu-create-index-function #'ggtags-build-imenu-index)
+
+;; Semantic check
+(require 'semantic)
+(require 'semantic/ia)
+(require 'semantic/bovine/gcc)
+
+(defun my-semantic-hook ()
+  (imenu-add-to-menubar "TAGS"))
+(add-hook 'semantic-init-hooks 'my-semantic-hook)
+
+(global-semanticdb-minor-mode 1)
+(global-semantic-idle-scheduler-mode 1)
+
+(semantic-mode 1)
