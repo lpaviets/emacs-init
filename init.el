b55ed7e301fbd3d5d805ae6b32061147a206a6ff
paviets201 <paviets201@n302l-g15p11.png.unicaen.fr>
2021-02-04 15:52:02 +0100

    Refactored using use-package

--- init.el
+++ init.el
@@ -19,33 +19,24 @@
 ;; __________________________________________
 
 
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-
-(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
-
+;; Initialize package sources
 (require 'package)
-(setq package-enable-at-startup nil)
 
-(add-to-list 'package-archives
-	     '("gnu" . "https://elpa.gnu.org/packages/") t)
-(add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/") t)
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
 
 (package-initialize)
 
-(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
-                    (not (gnutls-available-p))))
-       (proto (if no-ssl "http" "https")))
-  (when no-ssl
-    (warn "\ Your version of Emacs does not support SSL
-connections, which is unsafe because it allows man-in-the-middle
-attacks.  There are two things you can do about this warning:
-1. Install an Emacs version that does support SSL and be safe.
-2. Remove this warning from your init file so you won't see it
-again.")))
+(unless package-archive-contents
+ (package-refresh-contents))
+
+;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+   (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
 
 
 ;; __________________________________________
@@ -56,39 +47,10 @@ again.")))
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#e090d7" "#8cc4ff" "#eeeeec"])
- '(custom-enabled-themes (quote (tsdh-dark)))
- '(fci-rule-color "#5B6268")
- '(jdee-db-active-breakpoint-face-colors (cons "#2b2a27" "#ff5d38"))
- '(jdee-db-requested-breakpoint-face-colors (cons "#2b2a27" "#98be65"))
- '(jdee-db-spec-breakpoint-face-colors (cons "#2b2a27" "#3f444a"))
- '(objed-cursor-color "#ff5d38")
  '(package-selected-packages
    (quote
-    (flycheck company-box jedi yasnippet-snippets yasnippet company-c-headers company-irony company function-args ggtags magit ac-math which-key pdf-tools auctex gnu-elpa-keyring-update sage-shell-mode doom-themes realgud realgud-ipdb auto-complete ##)))
- '(vc-annotate-background "#2b2a27")
- '(vc-annotate-color-map
-   (list
-    (cons 20 "#98be65")
-    (cons 40 "#a4c551")
-    (cons 60 "#b0cc3d")
-    (cons 80 "#bcd42a")
-    (cons 100 "#c1a623")
-    (cons 120 "#c5781c")
-    (cons 140 "#cb4b16")
-    (cons 160 "#c95a58")
-    (cons 180 "#c7699a")
-    (cons 200 "#c678dd")
-    (cons 220 "#d96fa6")
-    (cons 240 "#ec666f")
-    (cons 260 "#ff5d38")
-    (cons 280 "#cf563c")
-    (cons 300 "#9f5041")
-    (cons 320 "#6f4a45")
-    (cons 340 "#5B6268")
-    (cons 360 "#5B6268")))
- '(vc-annotate-very-old-color nil))
+    (flycheck company-box jedi yasnippet-snippets yasnippet company function-args ggtags magit ac-math which-key pdf-tools auctex gnu-elpa-keyring-update sage-shell-mode doom-themes  auto-complete ##)))
+)
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -96,38 +58,110 @@ again.")))
  ;; If there is more than one, they won't work right.
  )
 
-(load-theme 'doom-Iosvkem)
-
 
 
 ;;__________________________________________
 ;; Global "comfort" modes and shortcuts
 ;; Modes:
 (delete-selection-mode 1)
-(setq inhibit-startup-screen t)
-;; Shortcuts
 
-(global-set-key (kbd "C-x C-b") 'ibuffer)
+(setq inhibit-startup-message t)
 
-(setq undo-tree-visualizer-timestamps t)
-(global-undo-tree-mode)
+(scroll-bar-mode -1)        ; Disable visible scrollbar
+(tool-bar-mode -1)          ; Disable the toolbar
+(tooltip-mode -1)           ; Disable tooltips
+(set-fringe-mode 10)        ; Give some breathing room
+
+(menu-bar-mode -1)            ; Disable the menu bar
 
 ;; __________________________________________
 ;; Packages options and management
 
+;; undo-tree
+
+(use-package undo-tree
+  :config
+  (setq undo-tree-visualizer-timestamps t)
+  (global-undo-tree-mode))
+
 ;; auto-mode
 (add-to-list 'auto-mode-alist '("\\.g4\\'" . antlr-mode))
 
-;; Global linum-mode except in some major modes
-(require 'linum)
-(define-global-minor-mode my-global-linum-mode linum-mode
-  (lambda ()
-    (when (not (memq major-mode
-                     (list 'doc-view-mode
-			   'undo-tree-visualizer
-			   'pdf-view-mode))) ;; add other major modes in which to disable linum-mode
-      (linum-mode))))
-(my-global-linum-mode t)
+
+;; Global line numbering mode, except in some major modes
+(column-number-mode)
+(global-display-line-numbers-mode t)
+
+;; Disable line numbers for some modes
+(dolist (mode '(org-mode-hook
+                term-mode-hook
+                shell-mode-hook
+                eshell-mode-hook
+		doc-view-mode-hook
+		undo-tree-visualizer-hook
+		pdf-view-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+;; Ivy
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+	 :map ivy-minibuffer-map
+	 ("TAB" . ivy-alt-done)
+	 ("C-l" . ivy-alt-done)
+	 ("C-j" . ivy-next-line)
+	 ("C-k" . ivy-previous-line)
+	 :map ivy-switch-buffer-map
+	 ("C-k" . ivy-previous-line)
+	 ("C-l" . ivy-done)
+	 ("C-d" . ivy-switch-buffer-kill)
+	 :map ivy-reverse-i-search-map
+	 ("C-k" . ivy-previous-line)
+	 ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
+
+;; Themes
+(use-package doom-themes
+  :init (load-theme 'doom-Iosvkem t))
+
+
+;; rainbow-delimiters. Hightlights with the same colour matching parenthesis
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+
+;; which-key. Shows all the available key sequences after a prefix
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 1))
+
+;; Adds things to Ivy
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1))
+
+
+;; Counsel. Adds things to Ivy
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+         ("C-x b" . counsel-ibuffer)
+         ("C-x C-f" . counsel-find-file)
+         :map minibuffer-local-map
+         ("C-r" . 'counsel-minibuffer-history)))
+
+;; Helpful. Extra documentation when calling for help
+(use-package helpful
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
 
 ;; Autocompletion: auto-complete, yasnippet and company
 
@@ -136,65 +170,69 @@ again.")))
 ;; (yas-global-mode 1)
 
 ;; Auto-complete
-(require 'auto-complete)
-(require 'auto-complete-config)
-(setq ac-use-quick-help t)
-(setq-default ac-sources '(ac-source-yasnippet
+(use-package auto-complete
+  :config
+  (setq ac-use-quick-help t)
+  (setq-default ac-sources '(ac-source-yasnippet
 			   ac-source-words-in-same-mode-buffers
-			   ac-source-dictionary)) ; see auto-complete doc for other sources
+			   ac-source-dictionary))) ; see auto-complete doc for other sources
 
 ;; Company
-(require 'company)
-;;(require 'company-c-headers)
-(setq company-idle-delay 0)
-(setq company-minimum-prefix-length 3)
-(company-quickhelp-mode t)
-(setq company-quickhelp-delay 0)
+(use-package company
+  :config
+  (setq company-idle-delay 0)
+  (setq company-minimum-prefix-length 3)
+  (setq company-quickhelp-delay 0)
+  :config
+  (global-company-mode t))
 
-(add-hook 'after-init-hook 'global-company-mode)
+(use-package company-quickhelp
+  :init (company-quickhelp-mode t))
 
 ;; Use pdf-tools to open PDF files
 (pdf-tools-install)
 
 ;; Configure ggtags for C and C++
-(require 'cc-mode)
-(require 'ggtags)
+;; (require 'cc-mode)
+;; (require 'ggtags)
 
-(add-hook 'c-mode-common-hook
-	  (lambda ()
-	    (when (derived-mode-p 'c-mode 'c++-mode 'java-mode 'asm-mode)
-	      (ggtags-mode 1))))
+;; (add-hook 'c-mode-common-hook
+;; 	  (lambda ()
+;; 	    (when (derived-mode-p 'c-mode 'c++-mode 'java-mode 'asm-mode)
+;; 	      (ggtags-mode 1))))
 
-(define-key ggtags-mode-map (kbd "C-c g s") 'ggtags-find-other-symbol)
-(define-key ggtags-mode-map (kbd "C-c g h") 'ggtags-view-tag-history)
-(define-key ggtags-mode-map (kbd "C-c g r") 'ggtags-find-reference)
-(define-key ggtags-mode-map (kbd "C-c g f") 'ggtags-find-file)
-(define-key ggtags-mode-map (kbd "C-c g c") 'ggtags-create-tags)
-(define-key ggtags-mode-map (kbd "C-c g u") 'ggtags-update-tags)
+;; (define-key ggtags-mode-map (kbd "C-c g s") 'ggtags-find-other-symbol)
+;; (define-key ggtags-mode-map (kbd "C-c g h") 'ggtags-view-tag-history)
+;; (define-key ggtags-mode-map (kbd "C-c g r") 'ggtags-find-reference)
+;; (define-key ggtags-mode-map (kbd "C-c g f") 'ggtags-find-file)
+;; (define-key ggtags-mode-map (kbd "C-c g c") 'ggtags-create-tags)
+;; (define-key ggtags-mode-map (kbd "C-c g u") 'ggtags-update-tags)
 
-(define-key ggtags-mode-map (kbd "M-,") 'pop-tag-mark)
+;; (define-key ggtags-mode-map (kbd "M-,") 'pop-tag-mark)
 
-(setq-local imenu-create-index-function #'ggtags-build-imenu-index)
+;; (setq-local imenu-create-index-function #'ggtags-build-imenu-index)
 
 ;; ;; Semantic
-(require 'semantic)
+(use-package semantic
 ;; (require 'semantic/ia)
 ;; (require 'semantic/bovine/gcc)
 
 ;; (defun my-semantic-hook ()
 ;;   (imenu-add-to-menubar "TAGS"))
 ;; (add-hook 'semantic-init-hooks 'my-semantic-hook)
-
-(global-semanticdb-minor-mode t)
-(global-semantic-idle-scheduler-mode t)
-
-(semantic-mode t)
+  :config
+  (semantic-mode t)
+  (global-semanticdb-minor-mode t)
+  (global-semantic-idle-scheduler-mode t))
 
 
 ;; Flycheck
-(setq flycheck-relevant-error-other-file-show nil)
-(setq flycheck-indication-mode nil)
-(global-flycheck-mode t)
+(use-package flycheck
+  :init
+  (setq flycheck-relevant-error-other-file-show nil)
+  (setq flycheck-indication-mode nil)
+  :config
+  :hook (python-mode . flycheck-mode))
 
 ;; __________________________________________
 ;; Language specific configuration
@@ -225,7 +263,6 @@ again.")))
 
 (add-hook 'python-mode-hook 'my-python-hooks)
 ;; End Python mode
-
 
 ;;LaTeX
 
