36e6489c38e203a5abdc2c867a72cf2680bc8d82
paviets201 <paviets201@n302l-g15p11.png.unicaen.fr>
2021-02-04 16:26:26 +0100

    Added Magit. Removed useless bindings for Ivy. Small adjusts on company and flycheck.

--- init.el
+++ init.el
@@ -108,16 +108,12 @@
   :diminish
   :bind (("C-s" . swiper)
 	 :map ivy-minibuffer-map
-	 ("TAB" . ivy-alt-done)
+	 ("TAB" . ivy-partial-or-done)
 	 ("C-l" . ivy-alt-done)
-	 ("C-j" . ivy-next-line)
-	 ("C-k" . ivy-previous-line)
 	 :map ivy-switch-buffer-map
-	 ("C-k" . ivy-previous-line)
 	 ("C-l" . ivy-done)
 	 ("C-d" . ivy-switch-buffer-kill)
 	 :map ivy-reverse-i-search-map
-	 ("C-k" . ivy-previous-line)
 	 ("C-d" . ivy-reverse-i-search-kill))
   :config
   (ivy-mode 1))
@@ -182,9 +178,9 @@
 ;; Company
 (use-package company
   :config
-  (setq company-idle-delay 0)
+  (setq company-idle-delay 0.1)
   (setq company-minimum-prefix-length 3)
-  (setq company-quickhelp-delay 0)
+  (setq company-quickhelp-delay 1)
   (global-company-mode t)
   :diminish (company-mode))
 
@@ -235,7 +231,17 @@
   (setq flycheck-relevant-error-other-file-show nil)
   (setq flycheck-indication-mode nil)
   :config
-  :hook (python-mode . flycheck-mode))
+  :hook (python-mode . flycheck-mode)) ; Temporary to avoid noise ...
+
+;; Magit
+
+(use-package magit
+  ;; :custom (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1)
+
+  ;; uncomment previous line to have magit open itself within the same buffer
+  ;; instead of in another buffer  
+  )
+
 
 ;; __________________________________________
 ;; Language specific configuration
